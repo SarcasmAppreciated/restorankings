55f479d3c242665fe61d067bffcbd93e471675fe
Benson Li <bl4074@hotmail.com>
2024-03-10 20:44:59 -0700

    BBT

--- src/assets/BBT.jsx
+++ src/assets/BBT.jsx
@@ -1,5 +1,6 @@
 export const bbtFilterCategories = [
-    ["belltown"]
+    ["belltown", "cambie", "capitol hill", "cupertino", "downtown", "international district",
+    "mountain view", "mt pleasant", "palo alto", "richmond", "ubc", "union square", "uw"]
 ];
 
 export const bbt = [
@@ -18,7 +19,7 @@ export const bbt = [
         name: "Alutea",
         neighbourhood: "capitol hill",
         value: "high",
-        tags: ["capitol hill", "bbt", "other-drinks"],
+        tags: ["capitol hill", "bbt", "other drinks"],
         address: "1715 12th Ave #100, Seattle, WA 98122",
         notes: "Very aesthetic.",
         dateOfReview: "2022"
@@ -62,5 +63,195 @@ export const bbt = [
         address: "1416 10th Ave, Seattle, WA 98122",
         notes: "Paying for aesthetic.",
         dateOfReview: "2023"
+    },
+    {
+        id: "yifang_taiwan_fruit_tea",
+        name: "YiFang Taiwan Fruit Tea",
+        neighbourhood: "international district",
+        value: "medium",
+        tags: ["international district", "bbt", "fruit tea"],
+        address: "619 S King St, Seattle, WA 98104",
+        notes: "-",
+        dateOfReview: "2023"
+    },
+    {
+        id: "chatime",
+        name: "Chatime",
+        neighbourhood: "cambie",
+        value: "high",
+        tags: ["cambie", "bbt"],
+        address: "6361 Cambie St, Vancouver, BC V5Z 3B2, Canada",
+        notes: "Generic great chain.",
+        dateOfReview: "2022"
+    },
+    {
+        id: "bubble_queen",
+        name: "Bubble Queen",
+        neighbourhood: "richmond",
+        value: "high",
+        tags: ["richmond", "bbt"],
+        address: "8888 Odlin Crescent #1180, Richmond, BC V6X 3Z8, Canada",
+        notes: "Creative flavours.",
+        dateOfReview: "2022"
+    },
+    {
+        id: "dragon_ball",
+        name: "Dragon Ball",
+        neighbourhood: "cambie",
+        value: "high",
+        tags: ["cambie", "bbt", "fruit tea"],
+        address: "1007 W King Edward Ave, Vancouver, BC V6H 1Z3, Canada",
+        notes: "Best fruit BBT in Vancouver.",
+        dateOfReview: "2022"
+    },
+    {
+        id: "truedan",
+        name: "Truedan",
+        neighbourhood: "richmond",
+        value: "high",
+        tags: ["richmond", "bbt", "black sugar"],
+        address: "In side of Kanae Yakiniku, 4231 Hazelbridge Wy #205, Richmond, BC V6X 3L7, Canada",
+        notes: "Best black sugar bbt in Vancouver.",
+        dateOfReview: "2023"
+    },
+    {
+        id: "the_alley",
+        name: "The Alley",
+        neighbourhood: "downtown",
+        value: "medium",
+        tags: ["downtown", "bbt", "instagram"],
+        address: "985 Hornby St, Vancouver, BC V6Z 1V3, Canada",
+        notes: "Aesthetics.",
+        dateOfReview: "2022"
+    },
+    {
+        id: "sharetea",
+        name: "Sharetea",
+        neighbourhood: "international district",
+        value: "medium",
+        tags: ["international district", "bbt"],
+        address: "651 S King St, Seattle, WA 98104",
+        notes: "Chain.",
+        dateOfReview: "2022"
+    },
+    {
+        id: "sharetea",
+        name: "Sharetea",
+        neighbourhood: "international district",
+        value: "medium",
+        tags: ["international district", "bbt"],
+        address: "651 S King St, Seattle, WA 98104",
+        notes: "Chain.",
+        dateOfReview: "2022"
+    },
+    {
+        id: "tea_era",
+        name: "Tea Era",
+        neighbourhood: "mountain view",
+        value: "medium",
+        tags: ["mountain view", "bbt"],
+        address: "651 S King St, Seattle, WA 98104",
+        notes: "I love the Tie Guan Yin flavour.",
+        dateOfReview: "2019"
+    },
+    {
+        id: "coco",
+        name: "Coco",
+        neighbourhood: "bellevue",
+        value: "medium",
+        tags: ["bellevue", "bbt"],
+        address: "11028 NE 8th St, Bellevue, WA 98004",
+        notes: "Chain.",
+        dateOfReview: "2023"
+    },
+    {
+        id: "pearl_fever",
+        name: "Pearl Fever",
+        neighbourhood: "ubc",
+        value: "medium",
+        tags: ["ubc", "bbt"],
+        address: "2182 Western Pkwy, Vancouver, BC V6T 1W6, Canada",
+        notes: "Chain.",
+        dateOfReview: "2020"
+    },
+    {
+        id: "gong_cha",
+        name: "Gong Cha",
+        neighbourhood: "downtown",
+        value: "medium",
+        tags: ["downtown", "bbt"],
+        address: "823 Granville St, Vancouver, BC V6Z 1K7, Canada",
+        notes: "Chain.",
+        dateOfReview: "2020"
+    },
+    {
+        id: "happy_lemon",
+        name: "Happy Lemon",
+        neighbourhood: "slu",
+        value: "medium",
+        tags: ["slu", "bbt"],
+        address: "2245 8th Ave R1-1A, Seattle, WA 98121",
+        notes: "Chain.",
+        dateOfReview: "2023"
+    },
+    {
+        id: "oasis_tea_zone",
+        name: "Oasis Tea Zone",
+        neighbourhood: "international district",
+        value: "medium",
+        tags: ["international district", "bbt"],
+        address: "519 6th Ave S, Seattle, WA 98104",
+        notes: "Serves a lot of non-traditional stuff.",
+        dateOfReview: "2022"
+    },
+    {
+        id: "ten_ren_tea",
+        name: "Ten Ren Tea",
+        neighbourhood: "cupertino",
+        value: "medium",
+        tags: ["cupertino", "bbt"],
+        address: "10881 N Wolfe Rd, Cupertino, CA 95014",
+        notes: "-",
+        dateOfReview: "2022"
+    },
+    {
+        id: "kung_fu_tea",
+        name: "Kung Fu Tea",
+        neighbourhood: "palo alto",
+        value: "medium",
+        tags: ["palo alto", "bbt"],
+        address: "318 University Ave, Palo Alto, CA 94301",
+        notes: "-",
+        dateOfReview: "2018"
+    },
+    {
+        id: "boba_guys",
+        name: "Boba Guys",
+        neighbourhood: "union square",
+        value: "low",
+        tags: ["union square", "bbt"],
+        address: "429 Stockton St, San Francisco, CA 94108",
+        notes: "Pure hype.",
+        dateOfReview: "2018"
+    },
+    {
+        id: "tea_zone",
+        name: "Tea Zone",
+        neighbourhood: "union square",
+        value: "low",
+        tags: ["union square", "bbt"],
+        address: "429 Stockton St, San Francisco, CA 94108",
+        notes: "Pure hype.",
+        dateOfReview: "2018"
+    },
+    {
+        id: "onezo_tapioca",
+        name: "OneZo Tapioca 丸作",
+        neighbourhood: "mt pleasant",
+        value: "to try",
+        tags: ["mt pleasant", "bbt"],
+        address: "608 W Broadway, Vancouver, BC V5Z 1G2, Canada",
+        notes: "",
+        dateOfReview: ""
     },
 ];
