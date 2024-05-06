d70f17076aab0e9c9d0623ee75032fe40ed1b265
Benson Li <bl4074@hotmail.com>
2024-05-06 01:35:04 -0700

    Filtered Name of Ampersands

--- src/components/restaurant/MapIframe.jsx
+++ src/components/restaurant/MapIframe.jsx
@@ -1,4 +1,5 @@
 function MapIframe({name, address}) {
+    const filteredName = name.replace("&", "%26");
     const API_KEY = "AIzaSyBnqey4RiuVvC7Gvd_HgubzSHnggkvAJIc";
     return (
         <iframe
@@ -8,7 +9,7 @@ function MapIframe({name, address}) {
             loading="lazy"
             allowFullscreen
             referrerPolicy="no-referrer-when-downgrade"
-            src={`https://www.google.com/maps/embed/v1/place?key=${API_KEY}&q=${name},${address}`}>
+            src={`https://www.google.com/maps/embed/v1/place?key=${API_KEY}&q=${filteredName},${address}`}>
         </iframe>
     )
 }
